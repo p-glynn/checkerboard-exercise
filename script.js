89d4377673fe4d80dc07bb3a5e1ace3c9a612778
Patrick Glynn <pjglynn57@gmail.com>
2017-10-11 14:14:08 -0600

    randomizer working

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ for (i=0; i<81; i++) {
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
 
+  function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+  }
 
+  tile.style.backgroundColor = getRandomColor();
 
 }
