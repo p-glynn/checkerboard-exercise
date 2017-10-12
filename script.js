95cbd1d0b1ddf51437c06792ffc6b97e1331f98e
Patrick Glynn <pjglynn57@gmail.com>
2017-10-11 18:45:07 -0600

    trying to figure out gradient

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-var tileArr = [];
+// var tileArr = [];
 
 //initially populate the board
 
-for (i=0; i<81; i++) {
+for (let i=0; i<81; i++) {
   var tile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
   document.body.prepend(tile);
   tile.style.width = "11.1%";
-  // tile.display = "float";
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
-
   if (i%2 === 0) {
-    document.tile.style.backgroundColor = "white";
+    tile.style.backgroundColor = "white";
   } else {
-    document.tile.style.backgroundColor = "black";
+    tile.style.backgroundColor = "black";
   }
-
 }
