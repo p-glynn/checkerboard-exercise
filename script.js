7492cc1d04a111be925bcb99baf5bd0c74cc5142
Patrick Glynn <pjglynn57@gmail.com>
2017-10-12 15:53:08 -0600

    random gradient working

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 // var tileArr = [];
-
+// for (let i=0; i<81; i++) {
+//   tileArr.push(randomColor())
+// }
 //initially populate the board
 
 for (let i=0; i<81; i++) {
   var tile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
-  document.body.prepend(tile);
+  var indArr = [];
+  for (let j=0; j<5; j++) {
+    indArr.push(randomColor())
+  }
+  // var randomNum = Math.floor(Math.random*tileArr.length);
+  console.log(indArr);
+  document.body.append(tile);
   tile.style.width = "11.1%";
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
-   tile.style.background = `linear-gradient(to top left,${ranColorArray[0]},${ranColorArray[1]},${ranColorArray[2]},${ranColorArray[3]},${ranColorArray[4]})`;
-  // if (i%2 === 0) {
-  //   tile.style.backgroundColor = "white";
-  // } else {
-  //   tile.style.backgroundColor = "black";
-  // }
+  //
+  // // tile.style.backgroundColor = randomColor();
+  if (i%2===0) {
+    tile.style.background = `linear-gradient(to bottom left,${indArr[0]},${indArr[1]},${indArr[2]},${indArr[3]},${indArr[4]})`;
+  } else {
+    tile.style.background = `linear-gradient(to bottom right,${indArr[0]},${indArr[1]},${indArr[2]},${indArr[3]},${indArr[4]})`;
+  }
+
+}
+
+function randomColor() {
+var letters = '0123456789ABCDEF';
+var color = '#';
+for (var i = 0; i < 6; i++) {
+ color += letters[Math.floor(Math.random() * 16)];
+ }
+return color;
 }
