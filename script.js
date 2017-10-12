38a2a8fe8729fce94251b4a5cd6625cd1d1904f6
Patrick Glynn <pjglynn57@gmail.com>
2017-10-12 11:26:36 -0600

    added snippet from F

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ for (let i=0; i<81; i++) {
   tile.style.width = "11.1%";
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
-  if (i%2 === 0) {
-    tile.style.backgroundColor = "white";
-  } else {
-    tile.style.backgroundColor = "black";
-  }
+   tile.style.background = `linear-gradient(to top left,${ranColorArray[0]},${ranColorArray[1]},${ranColorArray[2]},${ranColorArray[3]},${ranColorArray[4]})`;
+  // if (i%2 === 0) {
+  //   tile.style.backgroundColor = "white";
+  // } else {
+  //   tile.style.backgroundColor = "black";
+  // }
 }
