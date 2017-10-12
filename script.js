3efeb08b3861a0d8b0c147c198717aefbcd8b051
Patrick Glynn <pjglynn57@gmail.com>
2017-10-11 18:58:10 -0600

    comments

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
+// tiles need to be in array to select a randomized index
+
 var tileArr = [];
 
 //initially populate the board
 
 for (i=0; i<1600; i++) {
-  var tile = document.createElement("div");
-  var body = document.getElementsByTagName("body")[0];
-  document.body.prepend(tile);
-  tile.style.width = "2%";
-  tile.display = "float";
-  tile.style.float = "left";
-  tile.style.paddingBottom = "2%";
-  tileArr.push(tile)
-  tile.style.backgroundColor = randomColor();
+ var tile = document.createElement("div");
+ var body = document.getElementsByTagName("body")[0];
+ document.body.prepend(tile);
+ tile.style.width = "2%";
+ tile.display = "float";
+ tile.style.float = "left";
+ tile.style.paddingBottom = "2%";
+ tileArr.push(tile)
+ tile.style.backgroundColor = randomColor();
 }
+
+
+// picks a random color for the tile
 
 function randomColor() {
 var letters = '0123456789ABCDEF';
 var color = '#';
 for (var i = 0; i < 6; i++) {
-  color += letters[Math.floor(Math.random() * 16)];
-  }
+ color += letters[Math.floor(Math.random() * 16)];
+ }
 return color;
 }
 
+// add tile array to document
+
 for(var i=0;i<tileArr.length;i++){
-  document.body.prepend(tileArr[i]);
+ document.body.prepend(tileArr[i]);
 }
 
+// makes a new color
+
 function tileReplace(){
-  var randIndex = Math.ceil(Math.random()*tileArr.length-1);
-  var newTileColor = tileArr[randIndex].style.backgroundColor = randomColor();
+ var randIndex = Math.ceil(Math.random()*tileArr.length-1);
+ var newTileColor = tileArr[randIndex].style.backgroundColor = randomColor();
 }
+
+
+//sets the frequency that colors change
 
 var intervalID = window.setInterval(tileReplace, .00001);
 
