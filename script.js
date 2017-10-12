b9578f4f06f4e79f783663e689023de5f2c566f5
Patrick Glynn <pjglynn57@gmail.com>
2017-10-12 16:18:33 -0600

    weird horizontal grad

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// var tileArr = [];
-// for (let i=0; i<81; i++) {
-//   tileArr.push(randomColor())
-// }
+var tileArr = [];
+for (let i=0; i<89; i++) {
+  tileArr.push(randomColor())
+}
 //initially populate the board
 
 for (let i=0; i<81; i++) {
   var tile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
-  var indArr = [];
-  for (let j=0; j<5; j++) {
-    indArr.push(randomColor())
-  }
+  // var indArr = [];
+  // for (let j=0; j<5; j++) {
+  //   indArr.push(randomColor())
+  // }
   // var randomNum = Math.floor(Math.random*tileArr.length);
-  console.log(indArr);
+  // console.log(indArr);
   document.body.append(tile);
   tile.style.width = "11.1%";
   tile.style.float = "left";
   tile.style.paddingBottom = "11.1%";
   //
   // // tile.style.backgroundColor = randomColor();
-  if (i%2===0) {
-    tile.style.background = `linear-gradient(to bottom left,${indArr[0]},${indArr[1]},${indArr[2]},${indArr[3]},${indArr[4]})`;
-  } else {
-    tile.style.background = `linear-gradient(to bottom right,${indArr[0]},${indArr[1]},${indArr[2]},${indArr[3]},${indArr[4]})`;
-  }
+  tile.style.background = `linear-gradient(to top,${tileArr[i]},${tileArr[i+1]},${tileArr[i+2]},${tileArr[i+3]},${tileArr[i+4]},${tileArr[i+5]})`;
+  // if (i%2===0) {
+  //   tile.style.background = `linear-gradient(to bottom left,${indArr[0]},${indArr[1]},${indArr[2]},${indArr[3]},${indArr[4]})`;
+  // } else {
+  //   tile.style.background = `linear-gradient(to bottom right,${indArr[0]},${indArr[1]},${indArr[2]},${indArr[3]},${indArr[4]})`;
+  // }
 
 }
 
